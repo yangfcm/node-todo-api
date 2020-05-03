c42d2d4ed00bfeae9fe2ee7c1bcf05d91d6c116e
Fan Yang <yangfcm16@gmail.com>
2020-05-03 23:48:46 +0930

    get rid of warnings

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.Promise = global.Promise;  // Use JS inherited Promise
+mongoose.Promise = global.Promise; // Use JS inherited Promise
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 module.exports = mongoose;
