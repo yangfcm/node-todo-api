b1316d960cb7e2bc303e5edf42524c91aa7acd15
yangfcm <yangfcm@outlook.com>
2023-02-23 14:05:29 +1030

    Refactor error message to constants.

---
+++ src/config/constants.ts
@@ -0,0 +1,2 @@
+export const EMAIL_IS_REQUIRED = "Email is required.";
+export const USERNAME_IS_REQUIRED = "Username is required.";
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
+import { USERNAME_IS_REQUIRED, EMAIL_IS_REQUIRED } from "../config/constants";
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     trim: true,
-    required: true,
+    required: [true, EMAIL_IS_REQUIRED],
     unique: true,
   },
   username: {
     type: String,
     trim: true,
-    required: [true, "User name is required!"],
+    required: [true, USERNAME_IS_REQUIRED],
     unique: true,
   },
   password: String,
