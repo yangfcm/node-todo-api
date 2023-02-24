83d72512f71c84cec8403ccfb825042fa9e1b727
yangfcm <yangfcm@outlook.com>
2023-02-24 17:07:00 +1030

    Add timestamp to user schema.

--- src/models/user.ts
+++ src/models/user.ts
@@ -8,28 +8,38 @@ export interface IUser extends Document {
   email: string;
   username: string;
   password: string;
+  created_at: Date;
+  updated_at: Date;
   toUserResponse: () => UserResponse;
 }
 
-const userSchema = new mongoose.Schema<IUser>({
-  email: {
-    type: String,
-    trim: true,
-    required: [true, EMAIL_IS_REQUIRED],
-    unique: true,
-    validate: {
-      validator: (value: string) => isValidEmail(value),
-      message: "{VALUE} is not a valid email.",
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: [true, EMAIL_IS_REQUIRED],
+      unique: true,
+      validate: {
+        validator: (value: string) => isValidEmail(value),
+        message: "{VALUE} is not a valid email.",
+      },
     },
+    username: {
+      type: String,
+      trim: true,
+      required: [true, USERNAME_IS_REQUIRED],
+      unique: true,
+    },
+    password: String,
   },
-  username: {
-    type: String,
-    trim: true,
-    required: [true, USERNAME_IS_REQUIRED],
-    unique: true,
-  },
-  password: String,
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 userSchema.pre("save", async function () {
   const user = this;
